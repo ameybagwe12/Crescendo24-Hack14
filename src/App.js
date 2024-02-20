dffc112afa5ffd896b3c7ebfa8ff124622c0d243
Vailantan <vailantan@3003.com>
2024-02-20 23:19:51 +0530

    table

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import Analysis from "./components/analysis";
 import Navbar from "./components/navbar";
-
+import BasicTable from "./components/Table1";
 function App() {
   return (
     <>
@@ -10,6 +10,7 @@ function App() {
         style={{ backgroundColor: "#010E1C", minHeight: "100vh" }}
       >
         <Analysis />
+<BasicTable/>
       </div>
     </>
   );
