b26b3079cdcf2436d44476e5c338791d34f0e607
Amey Bagwe <ameybagwe09@gmail.com>
2024-02-20 22:03:13 +0530

    React Front page done

--- src/components/analysis.jsx
+++ src/components/analysis.jsx
@@ -4,13 +4,24 @@ import { Player } from "@lottiefiles/react-lottie-player";
 
 export default function Analysis() {
   return (
-    <div className="mainPic">
-      <Player
-        autoplay
-        loop
-        src={require("./my_assets/AnimationNav.json")}
-        style={{ height: "30%", width: "50%" }}
-      ></Player>
-    </div>
+    <>
+      <div className="mainPic">
+        <Player
+          autoplay
+          loop
+          src={require("./my_assets/AnimationNav.json")}
+          style={{ height: "30%", width: "50%" }}
+        ></Player>
+      </div>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <hr style={{ color: "white", paddingRight: 700, paddingLeft: 700 }} />
+      </div>
+    </>
   );
 }
